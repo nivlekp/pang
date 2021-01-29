d492eba8a9e00d985e135568f6874ada209c7528
nivlekp <osamupang@gmail.com>
2021-01-29 20:17:16 +1100

    updated setup.py and added GitHub Actions

--- setup.py
+++ setup.py
@@ -9,6 +9,17 @@ if __name__ == "__main__":
             "abjad",
             "abjad-ext-nauert",
         ],
+        extras_require={
+            "test": [
+                "black>=20.8b1",
+                "flake8>=3.8.2",
+                "isort>=4.3.21",
+                "mypy>=0.770",
+                "pytest>=5.4.3",
+                "pytest-cov>=2.6.0",
+                "pytest-helpers-namespace",
+            ]
+        },
         name="pang",
         packages=["pang"],
         version="0.1",
