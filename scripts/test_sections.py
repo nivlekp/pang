6b9ecbba838bb63f63b2d08c908717d45bf67893
nivlekp <osamupang@gmail.com>
2022-09-16 22:49:24 +1000

    add metadata test to test_sections.py

--- scripts/test_sections.py
+++ scripts/test_sections.py
@@ -11,9 +11,22 @@ def test_one_section(section_path):
     assert music_ly_path.exists()
     music_ly_bak_path = section_path / "music.ly.bak"
     music_ly_path.rename(music_ly_bak_path)
+
+    metadata_json_path = section_path / "__metadata__.json"
+    assert metadata_json_path.exists()
+    metadata_json_bak_path = section_path / "__metadata__.json.bak"
+    metadata_json_path.rename(metadata_json_bak_path)
+
     pang.build.run_music_py(section_path)
+
     new_music_ly_path = section_path / "new_music.ly"
     music_ly_path.rename(new_music_ly_path)
     music_ly_bak_path.rename(music_ly_path)
     assert filecmp.cmp(new_music_ly_path, music_ly_path, shallow=False)
     new_music_ly_path.unlink()
+
+    new_metadata_json_path = section_path / "new___metadata__.json"
+    metadata_json_path.rename(new_metadata_json_path)
+    metadata_json_bak_path.rename(metadata_json_path)
+    assert filecmp.cmp(new_metadata_json_path, metadata_json_path, shallow=False)
+    new_metadata_json_path.unlink()
