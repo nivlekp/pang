15b74f142634d1e491bf72f297b12fb3e5c3e531
nivlekp <osamupang@gmail.com>
2022-09-20 16:53:57 +1000

    ask CI to check music.ily

--- scripts/test_sections.py
+++ scripts/test_sections.py
@@ -12,6 +12,11 @@ def test_one_section(section_path):
     music_ly_bak_path = section_path / "music.ly.bak"
     music_ly_path.rename(music_ly_bak_path)
 
+    music_ily_path = section_path / "music.ily"
+    assert music_ily_path.exists()
+    music_ily_bak_path = section_path / "music.ily.bak"
+    music_ily_path.rename(music_ily_bak_path)
+
     metadata_json_path = section_path / "__metadata__.json"
     assert metadata_json_path.exists()
     metadata_json_bak_path = section_path / "__metadata__.json.bak"
@@ -24,6 +29,12 @@ def test_one_section(section_path):
     music_ly_bak_path.rename(music_ly_path)
     assert filecmp.cmp(new_music_ly_path, music_ly_path, shallow=False)
     new_music_ly_path.unlink()
+
+    new_music_ily_path = section_path / "new_music.ily"
+    music_ily_path.rename(new_music_ily_path)
+    music_ily_bak_path.rename(music_ily_path)
+    assert filecmp.cmp(new_music_ily_path, music_ily_path, shallow=False)
+    new_music_ily_path.unlink()
 
     new_metadata_json_path = section_path / "new___metadata__.json"
     metadata_json_path.rename(new_metadata_json_path)
