ec0415b2b49fc77b415bb050114660bfc7ee85ed
nivlekp <osamupang@gmail.com>
2022-10-08 23:31:31 +1100

    remove unused variable

--- scripts/test_score.py
+++ scripts/test_score.py
@@ -5,7 +5,6 @@ import pang
 
 def test_score():
     score_directory = pang.get_score_directory()
-    score_ly = score_directory / "score.ly"
     score_pdf = score_directory / "score.pdf"
     args = ["make", "score"]
     subprocess.run(args, check=True)
