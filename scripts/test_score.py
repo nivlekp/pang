0b0887debac9ee5bdd2ecfbc26ec9a7de89448b8
nivlekp <osamupang@gmail.com>
2022-10-08 23:34:37 +1100

    move the existing pdf to a new location before testing the score

--- scripts/test_score.py
+++ scripts/test_score.py
@@ -6,6 +6,8 @@ import pang
 def test_score():
     score_directory = pang.get_score_directory()
     score_pdf = score_directory / "score.pdf"
+    new_score_pdf = score_directory / "new_score.pdf"
+    score_pdf.rename(new_score_pdf)
     args = ["make", "score"]
     subprocess.run(args, check=True)
     assert score_pdf.exists()
