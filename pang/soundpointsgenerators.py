e24c1e05afaafeadee872b7d4d9cf980d3c1ec10
Tsz Kiu Pang <38950857+nivlekp@users.noreply.github.com>
2023-12-31 17:09:12 +1100

    reformat docstrings (#28)

--- pang/soundpointsgenerators.py
+++ pang/soundpointsgenerators.py
@@ -116,26 +116,30 @@ class AtaxicSoundPointsGenerator(SoundPointsGenerator):
             \new Voice
             {
                 {
-                    \tempo 4=60
                     %%% \time 4/4 %%%
+                    \tempo 4=60
                     r8
                     a'8
-                    \times 2/3 {
-                        \times 4/5 {
+                    \times 2/3
+                    {
+                        \times 4/5
+                        {
                             r32
                             c'16
                             ~
                             c'16
                             ~
                         }
-                        \times 2/3 {
+                        \times 2/3
+                        {
                             c'16
                             r8
                         }
                         r8
                     }
                     r4
-                    \times 2/3 {
+                    \times 2/3
+                    {
                         r8
                         r16.
                         f'32
@@ -229,8 +233,8 @@ class ManualSoundPointsGenerator(SoundPointsGenerator):
             \new Voice
             {
                 {
-                    \tempo 4=60
                     %%% \time 4/4 %%%
+                    \tempo 4=60
                     c'4
                     c'4
                     c'8
@@ -280,8 +284,8 @@ class ManualSoundPointsGenerator(SoundPointsGenerator):
             \new Voice
             {
                 {
-                    \tempo 4=60
                     %%% \time 4/4 %%%
+                    \tempo 4=60
                     c'4
                     cs'4
                     <d' ef'>8
@@ -345,26 +349,30 @@ class RandomWalkSoundPointsGenerator(SoundPointsGenerator):
             \new Voice
             {
                 {
-                    \tempo 4=60
                     %%% \time 4/4 %%%
+                    \tempo 4=60
                     r8
                     bf'8
-                    \times 2/3 {
-                        \times 4/5 {
+                    \times 2/3
+                    {
+                        \times 4/5
+                        {
                             r32
                             a'16
                             ~
                             a'16
                             ~
                         }
-                        \times 2/3 {
+                        \times 2/3
+                        {
                             a'16
                             r8
                         }
                         r8
                     }
                     r4
-                    \times 2/3 {
+                    \times 2/3
+                    {
                         r8
                         r16.
                         bf'32
@@ -460,26 +468,30 @@ class GRWSoundPointsGenerator(SoundPointsGenerator):
             \new Voice
             {
                 {
-                    \tempo 4=60
                     %%% \time 4/4 %%%
+                    \tempo 4=60
                     r8
                     bf'8
-                    \times 2/3 {
-                        \times 4/5 {
+                    \times 2/3
+                    {
+                        \times 4/5
+                        {
                             r32
                             bf'16
                             ~
                             bf'16
                             ~
                         }
-                        \times 2/3 {
+                        \times 2/3
+                        {
                             bf'16
                             r8
                         }
                         r8
                     }
                     r4
-                    \times 2/3 {
+                    \times 2/3
+                    {
                         r8
                         r16.
                         a'32
