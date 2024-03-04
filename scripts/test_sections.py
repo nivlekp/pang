17eab225ddb960bfe8fc23781bb4703857ba348d
Tsz Kiu Pang <38950857+nivlekp@users.noreply.github.com>
2024-03-04 23:43:25 +1100

    adds some logs to see the diff (#30)

--- scripts/test_sections.py
+++ scripts/test_sections.py
@@ -1,8 +1,19 @@
+import difflib
 import filecmp
+import sys
 
 import pytest
 
 import pang
+
+
+def print_diff(path0, path1):
+    with open(path0, "r") as fp:
+        lines0 = fp.readlines()
+    with open(path1, "r") as fp:
+        lines1 = fp.readlines()
+    diff = difflib.context_diff(lines0, lines1, fromfile="generated", tofile="expected")
+    sys.stdout.writelines(diff)
 
 
 @pytest.mark.parametrize("section_path", pang.get_section_paths())
@@ -27,17 +38,23 @@ def test_one_section(section_path):
     new_music_ly_path = section_path / "new_music.ly"
     music_ly_path.rename(new_music_ly_path)
     music_ly_bak_path.rename(music_ly_path)
-    assert filecmp.cmp(new_music_ly_path, music_ly_path, shallow=False)
+    assert filecmp.cmp(new_music_ly_path, music_ly_path, shallow=False), print_diff(
+        new_music_ly_path, music_ly_path
+    )
     new_music_ly_path.unlink()
 
     new_music_ily_path = section_path / "new_music.ily"
     music_ily_path.rename(new_music_ily_path)
     music_ily_bak_path.rename(music_ily_path)
-    assert filecmp.cmp(new_music_ily_path, music_ily_path, shallow=False)
+    assert filecmp.cmp(new_music_ily_path, music_ily_path, shallow=False), print_diff(
+        new_music_ily_path, music_ily_path
+    )
     new_music_ily_path.unlink()
 
     new_metadata_json_path = section_path / "new___metadata__.json"
     metadata_json_path.rename(new_metadata_json_path)
     metadata_json_bak_path.rename(metadata_json_path)
-    assert filecmp.cmp(new_metadata_json_path, metadata_json_path, shallow=False)
+    assert filecmp.cmp(
+        new_metadata_json_path, metadata_json_path, shallow=False
+    ), print_diff(new_metadata_json_path, metadata_json_path)
     new_metadata_json_path.unlink()
