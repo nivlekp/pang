69c22dc2e7f63bea161e9bf0382a92c7599c2a7e
nivlekp <osamupang@gmail.com>
2024-06-23 22:38:07 +1000

    removes non-existent fixture

--- scripts/test_score.py
+++ scripts/test_score.py
@@ -3,7 +3,7 @@ import tempfile
 import pang
 
 
-def test_score(score_pdf):
+def test_score():
     with tempfile.TemporaryDirectory() as tmpdir:
         output_file = pang.build.score(tmpdir)
         assert output_file.exists()
