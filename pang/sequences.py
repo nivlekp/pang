11b1e831debc23a5c51171418e5cdc5999122207
nivlekp <osamupang@gmail.com>
2024-07-29 22:14:02 +1000

    extracting method

--- pang/sequences.py
+++ pang/sequences.py
@@ -1,7 +1,5 @@
 import bisect
-import queue
 
-from .noteserver import NoteServer, _get_closest_server
 from .soundpointsgenerators import ManualSoundPointsGenerator, SoundPointsGenerator
 
 
@@ -13,10 +11,8 @@ class Sequence:
     def __init__(
         self,
         sound_points_generator=None,
-        nservers=1,
         sequence_duration=0,
     ):
-        self._servers = [NoteServer() for _ in range(nservers)]
         if sound_points_generator is None:
             sound_points_generator = ManualSoundPointsGenerator()
         assert isinstance(sound_points_generator, SoundPointsGenerator)
@@ -125,51 +121,6 @@ class Sequence:
         for sound_point in sequence:
             sound_point.instance += offset
         self._sound_points[index:index] = sequence._sound_points
-
-    def simulate_queue(self):
-        """
-        Simulate the queue based on the queue type.
-        """
-        # TODO: model rest_threshold
-        assert self.instances is not None and len(self.instances) > 0
-        servers = self._servers
-        curr_time = 0.0
-        q: queue.Queue = queue.Queue()
-        arrival_index = 0
-        while arrival_index < len(self.instances) or not q.empty():
-            server_index, closest_offset_instance = _get_closest_server(servers)
-            if q.empty():
-                if closest_offset_instance > self.instances[arrival_index]:
-                    # previous note has not finished yet, so we should queue
-                    # the newly arrived note
-                    q.put(arrival_index)
-                    curr_time = self.instances[arrival_index]
-                    arrival_index = arrival_index + 1
-                else:
-                    curr_time = self.instances[arrival_index]
-                    servers[server_index].serve(
-                        curr_time,
-                        sound_point=self._sound_points[arrival_index],
-                    )
-                    arrival_index = arrival_index + 1
-            else:  # there's already a client in the queue
-                # queue the current note
-                if (
-                    arrival_index < len(self.instances)
-                    and closest_offset_instance > self.instances[arrival_index]
-                ):
-                    q.put(arrival_index)
-                    curr_time = self.instances[arrival_index]
-                    arrival_index = arrival_index + 1
-                else:
-                    index = q.get()
-                    curr_time = closest_offset_instance
-                    servers[server_index].serve(
-                        curr_time,
-                        sound_point=self._sound_points[index],
-                        # duration=self.durations[index],
-                        # pitch=self.pitches[index]
-                    )
 
     def superpose(self, offset, sequence):
         """
