6c2256000a38214daa408387eb35faad1f6dcb10
nivlekp <osamupang@gmail.com>
2024-07-31 18:32:23 +1000

    rename tests name

--- tests/test_simulate_queue.py
+++ tests/test_simulate_queue.py
@@ -3,7 +3,7 @@ import numpy as np
 import pang
 
 
-def test_Sequence_simulate_queue_00():
+def test_simulate_queue_00():
     instances = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     durations = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
     sound_points_generator = pang.ManualSoundPointsGenerator(
@@ -65,7 +65,7 @@ def test_Sequence_simulate_queue_00():
     ]
 
 
-def test_Sequence_simulate_queue_01():
+def test_simulate_queue_01():
     instances = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     durations = [2.1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
     sound_points_generator = pang.ManualSoundPointsGenerator(
@@ -121,7 +121,7 @@ def test_Sequence_simulate_queue_01():
     ]
 
 
-def test_Sequence_simulate_queue_02():
+def test_simulate_queue_02():
     instances = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     durations = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
     sound_points_generator = pang.ManualSoundPointsGenerator(
@@ -186,7 +186,7 @@ def test_Sequence_simulate_queue_02():
     ]
 
 
-def test_Sequence_simulate_queue_03():
+def test_simulate_queue_03():
     instances = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     durations = [2.1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
     sound_points_generator = pang.ManualSoundPointsGenerator(
@@ -244,7 +244,7 @@ def test_Sequence_simulate_queue_03():
     ]
 
 
-def test_Sequence_simulate_queue_04():
+def test_simulate_queue_04():
     instances = [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
     durations = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
     sound_points_generator = pang.ManualSoundPointsGenerator(
