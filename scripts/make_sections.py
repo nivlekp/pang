95ac1fb00b0e8b1c6dba7a684d84c27f38758bb7
nivlekp <osamupang@gmail.com>
2024-08-07 22:06:05 +1000

    makes some scripts available

--- scripts/make_sections.py
+++ scripts/make_sections.py
@@ -3,16 +3,21 @@ import argparse
 import pang
 
 
-def main(sections):
-    section_paths = pang.get_section_paths(sections)
+def main():
+    parser = make_parser()
+    args = parser.parse_args()
+    section_paths = pang.get_section_paths(args.sections)
     for section_path in section_paths:
         print(f"Making Section {section_path.stem}...")
         pang.build.run_music_py(section_path)
         pang.build.run_lilypond_in_segment_directory(section_path)
 
 
-if __name__ == "__main__":
+def make_parser():
     parser = argparse.ArgumentParser(description="Make sections")
     parser.add_argument("--sections", action="extend", nargs="+", type=str)
-    args = parser.parse_args()
-    main(args.sections)
+    return parser
+
+
+if __name__ == "__main__":
+    main()
