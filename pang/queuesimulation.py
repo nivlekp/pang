99e32ad0ddb0676bcd3b6b9193bd933d6838ef18
nivlekp <osamupang@gmail.com>
2024-09-08 11:02:17 +1000

    removes one layer of if-else

--- pang/queuesimulation.py
+++ pang/queuesimulation.py
@@ -20,34 +20,26 @@ def simulate_queue(sequence: Sequence, servers: tuple[NoteServer, ...]):
     arrival_index = 0
     while arrival_index < len(sequence.instances) or queue:
         server_index, closest_offset_instance = _get_next_available_server(servers)
-        if not queue:
-            if closest_offset_instance > sequence.instances[arrival_index]:
-                # previous note has not finished yet, so we should queue
-                # the newly arrived note
-                queue.append(arrival_index)
-                curr_time = sequence.instances[arrival_index]
-                arrival_index = arrival_index + 1
-            else:
-                curr_time = sequence.instances[arrival_index]
-                servers[server_index].serve(
-                    curr_time,
-                    sound_point=sequence._sound_points[arrival_index],
-                )
-                arrival_index = arrival_index + 1
+        if (
+            arrival_index < len(sequence.instances)
+            and closest_offset_instance > sequence.instances[arrival_index]
+        ):
+            # previous note has not finished yet, so we should queue
+            # the newly arrived note
+            queue.append(arrival_index)
+            arrival_index = arrival_index + 1
+        elif not queue:
+            curr_time = sequence.instances[arrival_index]
+            servers[server_index].serve(
+                curr_time,
+                sound_point=sequence._sound_points[arrival_index],
+            )
+            arrival_index = arrival_index + 1
         else:  # there's already a client in the queue
-            # queue the current note
-            if (
-                arrival_index < len(sequence.instances)
-                and closest_offset_instance > sequence.instances[arrival_index]
-            ):
-                queue.append(arrival_index)
-                curr_time = sequence.instances[arrival_index]
-                arrival_index = arrival_index + 1
-            else:
-                index = queue.pop(0)
-                curr_time = closest_offset_instance
-                servers[server_index].serve(
-                    curr_time,
-                    sound_point=sequence._sound_points[index],
-                )
+            index = queue.pop(0)
+            curr_time = closest_offset_instance
+            servers[server_index].serve(
+                curr_time,
+                sound_point=sequence._sound_points[index],
+            )
     return servers
