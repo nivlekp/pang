bcf8a64283f88324848eaabf0aa558999a327065
nivlekp <osamupang@gmail.com>
2024-09-24 23:07:43 +1000

    extend shorter voice by rests

--- pang/aligner.py
+++ pang/aligner.py
@@ -2,14 +2,25 @@ import abjad
 
 
 def align_voices_length(voices: tuple[abjad.Voice, ...]) -> None:
-    longest_voice = _find_longest_voice(voices)
+    longest_voice = max(voices, key=_number_of_measures)
+    if len(longest_voice) == 0:
+        return
     for voice in voices:
         _align_two_voices(voice, longest_voice)
 
 
-def _find_longest_voice(voices: tuple[abjad.Voice, ...]) -> abjad.Voice:
-    return abjad.Voice()
+def _number_of_measures(voice: abjad.Voice) -> int:
+    if len(voice) == 0:
+        return 0
+    return abjad.get.measure_number(abjad.get.leaf(voice, -1))
 
 
 def _align_two_voices(shorter_voice: abjad.Voice, longer_voice: abjad.Voice) -> None:
-    pass
+    for measure in abjad.select.group_by_measure(longer_voice)[
+        _number_of_measures(shorter_voice) :
+    ]:
+        rest = abjad.MultimeasureRest(abjad.get.duration(measure))
+        time_signature = abjad.get.effective(measure[0], abjad.TimeSignature)
+        if time_signature is not None:
+            abjad.attach(time_signature, rest)
+        shorter_voice.extend([rest])
--- tests/test_aligner.py
+++ tests/test_aligner.py
@@ -2,14 +2,80 @@ import abjad
 from pang import aligner
 
 
-def test_align_voices_length_noop():
-    voices = (abjad.Voice("c'4"), abjad.Voice("c'4"))
+def test_align_voices_length_noop() -> None:
+    voices = (abjad.Voice(" ".join(["c'4"] * 4)), abjad.Voice(" ".join(["c'4"] * 4)))
     aligner.align_voices_length(voices)
-    assert all(len(voice) == 1 for voice in voices)
+    assert all(len(voice) == 4 for voice in voices)
 
 
-def test_align_voices_length_simple():
-    voices = (abjad.Voice("c'4 c'4"), abjad.Voice("c'4"))
+def test_align_voices_length_simple() -> None:
+    voices = (abjad.Voice(" ".join(["c'4"] * 8)), abjad.Voice(" ".join(["c'4"] * 4)))
     aligner.align_voices_length(voices)
-    assert len(voices[0]) == 2
-    assert len(voices[1]) == 2
+    assert abjad.lilypond(voices[0]) == abjad.string.normalize(
+        r"""
+        \new Voice
+        {
+            c'4
+            c'4
+            c'4
+            c'4
+            c'4
+            c'4
+            c'4
+            c'4
+        }
+        """
+    )
+    assert abjad.lilypond(voices[1]) == abjad.string.normalize(
+        r"""
+        \new Voice
+        {
+            c'4
+            c'4
+            c'4
+            c'4
+            R1
+        }
+        """
+    )
+
+
+def test_align_voices_length_varying_time_signature() -> None:
+    voices = (
+        abjad.Voice(r"\time 3/8 c'8 c'8 c'8 \time 2/4 c'4 c'4 \time 3/8 c'8 c'8 c'8"),
+        abjad.Voice(r"\time 3/8 c'8 c'8 r8"),
+    )
+    aligner.align_voices_length(voices)
+    assert abjad.lilypond(voices[0]) == abjad.string.normalize(
+        r"""
+        \new Voice
+        {
+            %%% \time 3/8 %%%
+            c'8
+            c'8
+            c'8
+            %%% \time 2/4 %%%
+            c'4
+            c'4
+            %%% \time 3/8 %%%
+            c'8
+            c'8
+            c'8
+        }
+        """
+    )
+    assert abjad.lilypond(voices[1]) == abjad.string.normalize(
+        r"""
+        \new Voice
+        {
+            %%% \time 3/8 %%%
+            c'8
+            c'8
+            r8
+            %%% \time 2/4 %%%
+            R2
+            %%% \time 3/8 %%%
+            R4.
+        }
+        """
+    )
