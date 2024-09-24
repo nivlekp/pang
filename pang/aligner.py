78d0590e38f26d54e0a8b47204938b7c74d8dfcd
nivlekp <osamupang@gmail.com>
2024-09-24 23:44:28 +1000

    only attach time signature if it is different from before

--- pang/aligner.py
+++ pang/aligner.py
@@ -16,11 +16,15 @@ def _number_of_measures(voice: abjad.Voice) -> int:
 
 
 def _align_two_voices(shorter_voice: abjad.Voice, longer_voice: abjad.Voice) -> None:
+    last_time_signature = abjad.get.effective(
+        abjad.get.leaf(shorter_voice, -1), abjad.TimeSignature
+    )
     for measure in abjad.select.group_by_measure(longer_voice)[
         _number_of_measures(shorter_voice) :
     ]:
         rest = abjad.MultimeasureRest(abjad.get.duration(measure))
         time_signature = abjad.get.effective(measure[0], abjad.TimeSignature)
-        if time_signature is not None:
+        if time_signature is not None and time_signature != last_time_signature:
             abjad.attach(time_signature, rest)
+            last_time_signature = time_signature
         shorter_voice.extend([rest])
--- tests/test_aligner.py
+++ tests/test_aligner.py
@@ -40,6 +40,43 @@ def test_align_voices_length_simple() -> None:
     )
 
 
+def test_align_voices_length_consistent_time_signature() -> None:
+    voice_0 = abjad.Voice("c'4 c'4 c'4 c'4 c'4 c'4")
+    voice_1 = abjad.Voice("c'4 c'4")
+    # put this in a staff group within a score to make time signatures visible
+    abjad.Score([abjad.StaffGroup([voice_0, voice_1])])
+    abjad.attach(abjad.TimeSignature((2, 4)), abjad.get.leaf(voice_0, 0))
+    abjad.attach(abjad.TimeSignature((2, 4)), abjad.get.leaf(voice_1, 0))
+    voices = (voice_0, voice_1)
+    aligner.align_voices_length(voices)
+    assert abjad.lilypond(voices[0]) == abjad.string.normalize(
+        r"""
+        \new Voice
+        {
+            \time 2/4
+            c'4
+            c'4
+            c'4
+            c'4
+            c'4
+            c'4
+        }
+        """
+    )
+    assert abjad.lilypond(voices[1]) == abjad.string.normalize(
+        r"""
+        \new Voice
+        {
+            \time 2/4
+            c'4
+            c'4
+            R2
+            R2
+        }
+        """
+    )
+
+
 def test_align_voices_length_varying_time_signature() -> None:
     voices = (
         abjad.Voice(r"\time 3/8 c'8 c'8 c'8 \time 2/4 c'4 c'4 \time 3/8 c'8 c'8 c'8"),
