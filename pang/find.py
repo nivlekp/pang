635716c0219d0a8637962e0c109afa5a201ae887
nivlekp <osamupang@gmail.com>
2025-03-10 13:42:04 +1100

    make method throw exception if not found

--- pang/find.py
+++ pang/find.py
@@ -5,9 +5,16 @@ import abjad
 T = TypeVar("T")
 
 
+class AttachmentNotFoundError(ValueError):
+    pass
+
+
 def q_event_attachment(leaf: abjad.Leaf, attachment_type: Type[T]) -> T:
-    return next(
+    attachments = [
         attachment
         for attachment in abjad.get.annotation(leaf, "q_event_attachments")
         if isinstance(attachment, attachment_type)
-    )
+    ]
+    if attachments:
+        return attachments[0]
+    raise AttachmentNotFoundError
--- tests/test_find.py
+++ tests/test_find.py
@@ -1,10 +1,26 @@
+import pytest
+
 import abjad
 import pang
 
 
-def test_find_q_event_attachment() -> None:
+def test_find_q_event_attachment__attachment_present() -> None:
     voice = abjad.Voice("c'4")
     leaf = abjad.get.leaf(voice, 0)
-    abjad.annotate(leaf, "q_event_attachments", [abjad.Dynamic("p"), abjad.ColorFingering(1)])
+    assert leaf is not None
+    abjad.annotate(
+        leaf, "q_event_attachments", [abjad.Dynamic("p"), abjad.ColorFingering(1)]
+    )
     assert pang.find.q_event_attachment(leaf, abjad.Dynamic) == abjad.Dynamic("p")
-    assert pang.find.q_event_attachment(leaf, abjad.ColorFingering) == abjad.ColorFingering(1)
+    assert pang.find.q_event_attachment(
+        leaf, abjad.ColorFingering
+    ) == abjad.ColorFingering(1)
+
+
+def test_find_q_event_attachment__attachment_absent() -> None:
+    voice = abjad.Voice("c'4")
+    leaf = abjad.get.leaf(voice, 0)
+    assert leaf is not None
+    abjad.annotate(leaf, "q_event_attachments", [abjad.ColorFingering(1)])
+    with pytest.raises(pang.find.AttachmentNotFoundError):
+        pang.find.q_event_attachment(leaf, abjad.Dynamic)
