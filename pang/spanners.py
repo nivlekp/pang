08591011a11216061233606da85e7b6d6e7ce609
nivlekp <osamupang@gmail.com>
2025-03-15 21:45:52 +1100

    remove unused variable

--- pang/spanners.py
+++ pang/spanners.py
@@ -4,7 +4,6 @@ import abjad
 def glissando_to_next_leaf(logical_tie: abjad.LogicalTie) -> None:
     all_leaves = abjad.select.with_next_leaf(logical_tie)
     logical_tie_leaves = all_leaves[:-1]
-    next_leaf = all_leaves[-1]
     _untie_logical_tie(logical_tie_leaves)
     abjad.glissando(all_leaves, allow_repeats=True, hide_middle_note_heads=True)
 
