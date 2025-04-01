a700905e69b363e4a05033c174d88c26e04279ad
nivlekp <osamupang@gmail.com>
2025-04-02 00:07:00 +1100

    update typing

--- pang/get.py
+++ pang/get.py
@@ -1,8 +1,8 @@
 import abjad
 
 
-def leaf(voice: abjad.Voice, index: int) -> abjad.Leaf:
-    leaf = abjad.get.leaf(voice, index)
+def leaf(argument: abjad.Voice | abjad.LogicalTie, index: int) -> abjad.Leaf:
+    leaf = abjad.get.leaf(argument, index)
     if leaf is None:
-        raise ValueError(f"Leaf not found in {voice}")
+        raise ValueError(f"Leaf not found in {argument}")
     return leaf
