2f39e7d7ba6734a4569acce917605e923b5ca9b6
nivlekp <osamupang@gmail.com>
2025-04-28 21:02:15 +1000

    add a unit test

---
+++ tests/test_spanners.py
@@ -0,0 +1,27 @@
+import abjad
+import pang
+
+
+def test_glissando_to_next_leaf() -> None:
+    voice = abjad.Voice("c'4~ c'4~ c'4 f'4")
+    pang.spanners.glissando_to_next_leaf(abjad.get.logical_tie(pang.get.leaf(voice, 0)))
+    assert abjad.lilypond(voice) == abjad.string.normalize(
+        r"""
+        \new Voice
+        {
+            c'4
+            \glissando
+            \hide NoteHead
+            \override Accidental.stencil = ##f
+            \override NoteColumn.glissando-skip = ##t
+            \override NoteHead.no-ledgers = ##t
+            c'4
+            c'4
+            \revert Accidental.stencil
+            \revert NoteColumn.glissando-skip
+            \revert NoteHead.no-ledgers
+            \undo \hide NoteHead
+            f'4
+        }
+        """
+    )
